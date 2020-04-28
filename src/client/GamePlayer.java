51926a74901db471f22df17d862d1785e3b904b1
ChemIsProgram <adslbbcc1949@gmail.com>
2020-04-28 20:24:48 +1000

    add comments.

--- src/client/GamePlayer.java
+++ src/client/GamePlayer.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  */
 public class GamePlayer {
 	
+	// declare for the host information
 	private final static String ADDRESS = "netprog1.csit.rmit.edu.au";
     private final static int PORT = 61618;
 
@@ -22,28 +23,42 @@ public class GamePlayer {
     public GamePlayer() {
     	
     	try {
+			// generate a new socket object with the address and port.
             connection = new Socket(ADDRESS, PORT);
 
+			// use the socket connection to get InputStream and OutputStream
+			// and convert to different object to operate them.
             printWriter = new PrintWriter(connection.getOutputStream(), true);
             scanner = new Scanner(connection.getInputStream());
             
-            String serverMessage = null;
+			String serverMessage = null;
+			
+			// use while loop to listen feedbacks from server.
             while (true) {
             	
             	try {
+					// read the message from server
             		serverMessage = scanner.nextLine();  
-                	
+					
+					// check whether or not the message is not empty.
+					// if the message is empty, continue the while loop.
                 	if (serverMessage == null || serverMessage.trim().length() == 0) {
                 		continue;
                 	}
-                	
-                	System.out.println(serverMessage);
+					
+					// print the message from server.
+					System.out.println(serverMessage);
+					
+					// check whether the message matches the end condition or not.
                 	if (serverMessage.startsWith("C") || serverMessage.startsWith("Sorry")) {
                 		break;
                 	}
-                	
+					
+					// get input from console and use PrintWriter to send the input to server.
                 	input = new Scanner(System.in);
-                	printWriter.println(input.nextLine());  
+					printWriter.println(input.nextLine()); 
+					
+				// If the server is shut down, end the while loop.
             	}catch (NoSuchElementException nsee) {
             		System.out.println("The server is out of service. No more games.");
             		break;
--- src/server/GameServer.java
+++ src/server/GameServer.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  */
 public class GameServer {
 	
+	// declare some default parameters
 	private final static int PORT = 61618;
 	
 	private final Integer MAX_CHANCES = 4;
@@ -31,24 +32,31 @@ public class GameServer {
 		ServerSocket server = null;
 
         try {
+			// start the server
             server = new ServerSocket(PORT);
             System.out.println("Game server is started.");
-            
+			
+			// use while loop to wait for player joining.
             while(true) {
             	
             	System.out.println("Waiting for new player...");
-                
+				
+				// accept a connection from client
                 connection = server.accept();
                 System.out.println("A new player has joined the game.");
-                
+				
+				// use the socket connection to get InputStream and OutputStream
+				// and convert to different object to operate them.
                 printWriter = new PrintWriter(connection.getOutputStream(),true);
                 connInput = new Scanner(connection.getInputStream());
                 
                 try {
+					// get player's name if it's a new player.
                 	if (playerName == null) {
                     	setPlayerName();
                     }
-                    
+					
+					// setup random number
                     if (randomNumber == null) {
                     	setRandomNumber();
                     }
@@ -56,40 +64,57 @@ public class GameServer {
                     printWriter.println("Let's start the game. Please guess a number between 0 and 12: ");
                     
                     String str = null;
-                    Integer guessChance = 1;
+					Integer guessChance = 1;
+					// use while loop to communicate with client continuously.
                     while (true) {
                     	try {
-                    		str = connInput.nextLine();  
+							// get the message from client
+							str = connInput.nextLine();  
+							
+							// check whether the input is a number or not.
                             Integer guessingNumber = null;
                             try {
-                            	guessingNumber = Integer.valueOf(str);
+								guessingNumber = Integer.valueOf(str);
+								
+							// if it is not a number, return the message to client.
                             }catch (NumberFormatException nfe) {
                             	printWriter.println("The input you entered is not a \"NUMBER\". Please give us a number: ");
                             	continue;
                             }
-                            
+							
+							// check whether the input number is in the range.
+							// if not, return the message to client.
                             if (guessingNumber < 0 || guessingNumber > 12) {
                             	printWriter.println("The number you entered is out of range. Please give us a number between 0 to 12: ");
                             	continue;
                             }
-                            
+							
+							// check whether the client is correct.
+							// if yes, return the message and end the while loop.
                             if (guessingNumber == randomNumber) {
                             	printWriter.println("Congratulations. You are right. The number is " + randomNumber + ".");
                             	reset();
                             	break;
                             }
-                            
+							
+							// check whether the client has other chances to guess.
+							// if not, return the message and end the while loop.
                             if ((MAX_CHANCES-guessChance) == 0) {
                             	printWriter.println("Sorry. The number you guess is " + guessingNumber + ", which is wrong. The target number is " + randomNumber + ". Thanks for joining the game.");
                             	reset();
                             	break;
                             }
-                            
+							
+							// returning different messages depends on different situations.
                             if (guessingNumber < randomNumber)
                             	printWriter.println("The number you guess is " + guessingNumber + ", which is smaller than the generated number. Please guess again (chances left: " + (MAX_CHANCES-guessChance) + "): ");
                             else
                             	printWriter.println("The number you guess is " + guessingNumber + ", which is bigger than the generated number. Please guess again (chances left: " + (MAX_CHANCES-guessChance) + "): ");
-                            guessChance ++;
+							
+							guessChance ++;
+
+						// if the client quits the game during game, reset all the parameters
+						// and wait for another player joining the game.
                     	}catch (NoSuchElementException nsee) {
                     		reset();
                     		break;
@@ -101,8 +126,6 @@ public class GameServer {
                 	reset();
                 	continue;
                 }
-                
-                
             }
             
         } catch (IOException e) {
@@ -115,17 +138,26 @@ public class GameServer {
 		
 	}
 
+	/**
+	 * reset the parameters to default values.
+	 */
 	private void reset() {
 		
 		playerName = null;
 		randomNumber = null;
 	}
 
+	/**
+	 * set random number to guess.
+	 */
 	private void setRandomNumber() {
 		randomNumber = new Random().nextInt(12);
 		System.out.println("The random number is " + randomNumber + ".");
 	}
 
+	/**
+	 * get player name from client
+	 */
 	private void setPlayerName() {
 		
 		printWriter.println("Welcome to the guessing game. Please give us your name: ");
